449122653d1fffbeae81f339f7f22c42b13bbfda
etienne gaillard <72806570+GlrdEtienne@users.noreply.github.com>
2024-01-27 02:34:11 +0100

    premier test

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "portfolio.html",
+    "styles.css",
+    // Add paths to all other HTML, JS, or TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
